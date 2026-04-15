a953afb6a876c1a2df4e375592b6c34a3f25a137
Gregouz <gregoire.riviere@epitech.eu>
2026-04-15 15:45:41 +0200

    feat: add main page with infos and edit page with button that allowe to came back to the main page and change the name

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,181 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ProfilPage(
-      Scaffold(
-        appBar: AppBar(
-          title: Text('Mon profil'),
+    return MaterialApp(home: ProfilPage());
+  }
+}
+
+class ProfilPage extends StatefulWidget {
+  const ProfilPage({super.key});
+
+  @override
+  State<ProfilPage> createState() => _ProfilPageState();
+}
+
+class _ProfilPageState extends State<ProfilPage> {
+  String name = "Emilien Arnaud";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 60, 54, 54),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 29, 158, 117),
+        title: Text('Mon profil', style: TextStyle(color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              CircleAvatar(
+                backgroundColor: const Color.fromARGB(255, 147, 239, 199),
+                radius: 100,
+                child: Text(
+                  'EA',
+                  style: TextStyle(
+                    fontSize: 80,
+                    color: Color.fromARGB(255, 25, 82, 42),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 12),
+
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+
+              SizedBox(height: 4),
+
+              Text(
+                'Développeur Roblox',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: const Color.fromARGB(255, 180, 180, 180),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: Divider(
+                  color: Colors.grey.shade700,
+                  thickness: 0.5,
+                  height: 30,
+                ),
+              ),
+
+              SizedBox(height: 14),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'À PROPOS',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 29, 158, 117),
+                    padding: EdgeInsets.symmetric(vertical: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final String? result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const EditProfilPage(),
+                      ),
+                    );
+                    if (result != null) {
+                      setState(() {
+                        name = result;
+                      });
+                    }
+                  },
+                  child: Text(
+                    'Modifier le profil',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      )
-    ),);
+      ),
+    );
+  }
+}
+
+class EditProfilPage extends StatelessWidget {
+  const EditProfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 60, 54, 54),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 29, 158, 117),
+        title: Text(
+          'Modifier mon profil',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 29, 158, 117),
+              ),
+              onPressed: () {
+                Navigator.pop(context, "Ilan Menerville");
+              },
+              child: Text("Sauvegarder", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
