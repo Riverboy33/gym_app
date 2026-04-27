3680ac7de94917e4a55efc5bf10af7e3538de26b
Gregouz <gregoire.riviere@epitech.eu>
2026-04-27 14:39:45 +0200

    feat: change colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color button = Color.fromARGB(255, 54, 73, 78);
+  static const Color button = Color.fromARGB(255, 57, 72, 77);
   static const Color backgroundSecondary = Color.fromARGB(255, 169, 206, 244);
   static const Color text = Colors.white;
   static const Color textSecondary = Color.fromARGB(255, 89, 112, 129);
-  static const Color background = Color.fromARGB(255, 126, 160, 183);
+  static const Color background = Color.fromARGB(255, 174, 203, 237);
 }
